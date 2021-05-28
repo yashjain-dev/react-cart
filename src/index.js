6c632e9fba838ad8b7f9752502f43e254642ce92
yashjain-dev <57740980+yashjain-dev@users.noreply.github.com>
2021-05-28 20:26:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBpV5RjjY4CCb95eNLQBppvN1f9DyQxlSw",
-  authDomain: "cart-13d12.firebaseapp.com",
-  projectId: "cart-13d12",
-  storageBucket: "cart-13d12.appspot.com",
-  messagingSenderId: "881861990787",
-  appId: "1:881861990787:web:6434070fa74e9a79447eb6"
-};
+
+
+
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
